da4e9187348f59b7b630cafaac5134d7fe80b095
CalebAvila03 <calebavila03@gmail.com>
2025-04-11 20:54:10 -0500

    taller

--- Taller SharedPreferences/TallerSharedPreferences/app/src/main/java/com/example/tallersharedpreferences/MusicaActivity.java
+++ Taller SharedPreferences/TallerSharedPreferences/app/src/main/java/com/example/tallersharedpreferences/MusicaActivity.java
@@ -1,6 +1,9 @@
 package com.example.tallersharedpreferences;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MusicaActivity extends AppCompatActivity {
 
+    Button btn_regresar_musica;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_musica);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn_regresar_musica = findViewById(R.id.btn_regresar_musica);
+        btn_regresar_musica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MusicaActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
+
     }
 }
