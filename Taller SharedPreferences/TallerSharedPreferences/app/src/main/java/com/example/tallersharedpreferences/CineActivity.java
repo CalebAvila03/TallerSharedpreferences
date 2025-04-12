3b24bfdd611fe05632baa0940092088b97b335b4
CalebAvila03 <calebavila03@gmail.com>
2025-04-11 20:45:07 -0500

    CineActivity.java

--- Taller SharedPreferences/TallerSharedPreferences/app/src/main/java/com/example/tallersharedpreferences/CineActivity.java
+++ Taller SharedPreferences/TallerSharedPreferences/app/src/main/java/com/example/tallersharedpreferences/CineActivity.java
@@ -1,6 +1,9 @@
 package com.example.tallersharedpreferences;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CineActivity extends AppCompatActivity {
 
+    Button btn_regresar_cine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cine);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn_regresar_cine = findViewById(R.id.btn_regresar_cine);
+        btn_regresar_cine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CineActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
+
     }
 }
