a1b0668c1f4c14234a87c001f4cdf3b3ea7f636d
CalebAvila03 <calebavila03@gmail.com>
2025-04-11 20:47:13 -0500

    taller

--- Taller SharedPreferences/TallerSharedPreferences/app/src/main/java/com/example/tallersharedpreferences/DeporteActivity.java
+++ Taller SharedPreferences/TallerSharedPreferences/app/src/main/java/com/example/tallersharedpreferences/DeporteActivity.java
@@ -1,7 +1,10 @@
 package com.example.tallersharedpreferences;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,10 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class DeporteActivity extends AppCompatActivity {
 
-    public static final String dataUserCache = "dataUser";
-    private static final int  mode_Private = Context.MODE_PRIVATE;
-    String dato1;
-    TextView txt_usuario_deporte;
+    Button btn_regresar_deporte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +23,15 @@ public class DeporteActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_deporte);
 
-        txt_usuario_deporte = findViewById(R.id.txt_usuario_deporte);
+        btn_regresar_deporte = findViewById(R.id.btn_regresar_deporte);
+        btn_regresar_deporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DeporteActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
